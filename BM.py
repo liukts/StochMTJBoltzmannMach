5eb50d2d37c0ec7ecd6e3e08d2f17180806d1f36
Paul <paulwbessler@utexas.edu>
2022-06-15 19:53:07 -0500

    Added Histogram

--- BM.py
+++ BM.py
@@ -1,53 +1,81 @@
 import numpy as np
 import random as rnd
+import matplotlib.pyplot as plt
+import os
 import copy
 
+# folder to save results
+date = "06_13_22"
+target_dir = ("RBM_Sim_" + date)
+
+# if folder does not exist, create it
+if not os.path.isdir("./outputs/"):
+    os.mkdir("./outputs/")
+if not os.path.isdir("./outputs/"):
+    os.mkdir("./outputs/")
+if not os.path.isdir("./outputs/"):
+    os.mkdir("./outputs/")
+
 #Boolean Clauses: (X||Y||Z), (X'||Y||Z), (X'||Y'||Z), (X||Y'||Z'), (X'||Y||Z')
-#Weight Matrix 
+#Weight Matrix (Created from Boolean Clauses)
 W = np.array([[-4, -1, -1, 10, -1, -1], [-1, -7, -2, -2, 10, -1], \
-     [-1, -2, -6, -2, -1, 10], [10, -2, -2, -8, -1, -1], \
-     [-1, 10, -1, -1, -5, -1], [-1, -1, 10, -1, -1, -5]])
+              [-1, -2, -6, -2, -1, 10], [10, -2, -2, -8, -1, -1], \
+              [-1, 10, -1, -1, -5, -1], [-1, -1, 10, -1, -1, -5]])
 
-
-#Initialize Gate Bias Voltage
-Vg = 20
-
-#Initialize Temperature & Step Size
+#Initialize Temperature & Step Size (Dictates the _____ of the model)
 Teff = 50
 step = 5
+Iter = 1000 #Number of Simulations to Run
+sols = [] #Empty array of solutions
 
-#Initialize Set Probability
-#Prob = 1/(1+np.exp(-(Vte - Vte0)/Teff))
-
-#Initialize Sigmoid
+#Initialize Sigmoid (This is what we use to determine probability of activation)
 def sigmoid(x):
     sig = 1/(1+np.exp(x))
     return sig
 
-#Initialize Neurons
+#Initialize Neurons (The variables that make up our Boolean Clauses)
 neurs = np.array([0,0,0,0,0,0])
-for h in range(0,6):
-    neurs[h] = rnd.randint(0, 1)
-print("Starting Solution:", neurs)
 
-#Initialize Energy
-temp = copy.deepcopy(neurs)
-curr = np.sum(np.array(np.transpose(neurs) * W * neurs))
-next = curr
+for f in range(0, Iter):
+    #Generate Random Values to Start 
+    for h in range(0,6):
+        neurs[h] = rnd.randint(0, 1)
 
-#Iterate until Temperature is 0
-while(Teff >= 0):
-    for h in range(0, 6):
-        temp[h] = (temp[h]+1) % 2
-        next = np.sum(np.transpose(temp) * W * temp)
-        rand = rnd.randint(0,10) / 10
-        if next < curr:
-            curr = next
-            neurs = copy.deepcopy(temp)
-        elif rand < sigmoid(-((Teff*0.16)-4)):
-            curr = next
-            neurs = copy.deepcopy(temp)
-        else:
-            temp = copy.deepcopy(neurs)
-        Teff -= step
-print("Ending Solution:  ", neurs)
+    Teff = 50 #Reset Temperature
+
+    #Initialize Energy 
+    temp = copy.deepcopy(neurs) #Temporary copy of neurons that holds the next possible solution
+    curr = np.sum(np.array(np.transpose(neurs) * W * neurs)) 
+    #Stores the calculated system energy of the current solution based on the weight matrix
+    next = curr #Temporary variable that holds the calculated energy of the next possible solution
+
+    #Iterate until the system has cooled
+    while(Teff >= 5):
+        for g in range(0,len(neurs)):
+            temp[g] = (temp[g]+1) % 2 #Flips one of the neurons (0 to 1 or 1 to 0)
+            next = np.sum(np.transpose(temp) * W * temp) #Calculates the energy of the next solution 
+            rand = rnd.uniform(0.00,1.00) #Generate a random number from 0.00 - 1.00
+            if next < curr: #If the next solution is better than the previous, save it
+                curr = next
+                neurs = copy.deepcopy(temp)
+            elif rand < sigmoid(-((Teff*0.16)-4)): 
+            #If the random number we generated is less than the number given from the sigmoid based on the temperature
+            #Then save this solution even though it is worse
+                curr = next
+                neurs = copy.deepcopy(temp)
+            else: #Reset temporary neurons
+                temp = copy.deepcopy(neurs)
+        Teff -= step 
+    
+    #Function to Convert Binary neurons to Decimal
+    sum = 0
+    for k in range(0, len(neurs)):
+        sum += (neurs[k] * (2**(len(neurs)-k-1)))
+    sols.append(sum) #Save Solution
+
+#Graphing of Histogram
+plt.hist(sols)
+plt.xlabel('Value')
+plt.ylabel('Frequencry')
+plt.title('Solution Frequency Over ' + str(Iter) + ' Iterations')
+plt.savefig('./outputs/' + target_dir + '_' + str(Iter) + '_Histogram.png')
