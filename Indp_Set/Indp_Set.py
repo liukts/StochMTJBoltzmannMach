e19b98f57b2572ae3eb8fd159258d454ce47d3e2
Paul <paulwbessler@utexas.edu>
2023-01-17 22:45:15 -0600

    Testing independent set, working on TSP

---
+++ Indp_Set/Indp_Set.py
@@ -0,0 +1,86 @@
+from re import S
+import numpy as np
+import random as rnd
+import matplotlib.pyplot as plt
+import os
+
+# folder to save results
+date = "1_17_23"
+target_dir = ("Indp_Sim_" + date)
+
+# if folder does not exist, create it
+if not os.path.isdir("./outputs/"):
+    os.mkdir("./outputs/")
+if not os.path.isdir("./outputs/"):
+    os.mkdir("./outputs/")
+if not os.path.isdir("./outputs/"):
+    os.mkdir("./outputs/")
+
+#Example Graph:
+#            0
+#           / \
+#          1   2 
+#         /\   /\
+#        /  \ /  \  
+#        \   4   /
+#         \ / \ /
+#          3   5
+
+
+
+#Solution is 101001 = 41
+#Graph G = (V, E) with edge set E and vertex set V
+Vertices = np.array([0,0,0,0,0,0])
+Edges = np.array([[10,1,1,-1,-1,-1], #Connections of node 0
+                  [1,10,-1,1,1,-1], #Connections of node 1
+                  [1,-1,10,-1,1,1], #Connections of node 2
+                  [-1,1,-1,10,1,-1], #Connections of node 3
+                  [-1,1,1,1,10,1], #Connections of node 4
+                  [-1,-1,1,-1,1,10]]) #Connections of node 5         
+
+#Initialize Temperature & Step Size (Dictates the stochasticity of the model)
+T_init = 10.00
+step = 0.01
+Iter = 1000 #Number of Simulations to Run
+sols = [] #Empty array of solutions
+
+#Initialize Sigmoid (This is what we use to determine probability of activation)
+def sigmoid(x):
+    sig = 1/(1+np.exp(x))
+    return sig
+
+for f in range(0, Iter):
+    #Generate Random Values to Start 
+    for h in range(0,len(Vertices)):
+        Vertices[h] = rnd.randint(0, 1)
+    weighted = np.dot(Vertices, Edges) #Calculate Weights
+
+    Teff = T_init #Reset Temperature    
+
+    #Iterate until the system has cooled
+    while(Teff >= 0.01):
+        for g in range(0, 1): #Iterations per temperature
+            for h in range(0,len(Vertices)): #Do this to each Neuron
+                rand = rnd.uniform(0, 1) #rand num for determining set probability
+                if rand < sigmoid(weighted[h]/Teff):
+                    Vertices[h] = 1
+                else:
+                    Vertices[h] = 0
+            weighted = np.dot(Vertices, Edges)
+        Teff -= step
+    
+    #Function to Convert Binary neurons to Decimal
+    sum = 0
+    for k in range(0, len(Vertices)):
+        sum += (Vertices[k] * (2**(len(Vertices)-k-1)))
+    print(Vertices)
+    sols.append(sum) #Save Solution
+
+#Graphing of Histogram
+plt.xticks(range(0, 64, 2))
+plt.yticks(range(0, 1000, 100))
+plt.hist(sols, bins=64)
+plt.xlabel('Value')
+plt.ylabel('Frequencry')
+plt.title('Solution Frequency Over ' + str(Iter) + ' Iterations')
+plt.savefig('./outputs/' + target_dir + '_' + str(Iter) + '_Histogram.png')
--- TSP/TSP.py
+++ TSP/TSP.py
@@ -35,11 +35,50 @@ if not os.path.isdir("./outputs/"):
 
 #Solution is A->B->C->D->A
                       # A  B  C  D  
-City_Dist = np.array([[ 0,20,42,35], #A
-                      [20, 0,30,34], #B
-                      [42,30, 0,12], #C
-                      [35,34,12, 0]])#D
+City_Dist = np.array([[-10,20,42,35], #A
+                      [20,-10,30,34], #B
+                      [42,30,-10,12], #C
+                      [35,34,12,-10]])#D
 City_Order = np.array([[0,0,0,0], #First City
                        [0,0,0,0], #Second City
                        [0,0,0,0], #Third City
                        [0,0,0,0]])#Fourth City
+NumCit = 4
+
+#Initialize Temperature & Step Size (Dictates the stochasticity of the model)
+T_init = 10.00
+step = 0.01
+Iter = 10 #Number of Simulations to Run
+sols = [] #Empty array of solutions
+
+#Initialize Sigmoid (This is what we use to determine probability of activation)
+def sigmoid(x):
+    sig = 1/(1+np.exp(x))
+    return sig
+
+for f in range(0, Iter):
+    #Generate Random Values to Start 
+    for h in range(0, NumCit):
+        for b in range(0, NumCit ):
+            City_Order[h][b] = rnd.randint(0, 1)
+    weighted = np.dot(City_Order, City_Dist) #Calculate Weights
+
+    Teff = T_init #Reset Temperature    
+
+    #Iterate until the system has cooled
+    while(Teff >= 1):
+        for g in range(0, 1): #Iterations per temperature
+            for h in range(0,NumCit): #Do this to each Neuron
+                for b in range(0, NumCit):
+                    rand = rnd.uniform(0, 1) #rand num for determining set probability
+                    if rand < sigmoid(weighted[h][b]/Teff):
+                        City_Order[h][b] = 1
+                    else:
+                        City_Order[h][b] = 0
+            weighted = np.dot(City_Order, City_Dist)
+        Teff -= step
+    
+    #Function to Convert Binary neurons to city order
+    print(City_Order)
+
+    
